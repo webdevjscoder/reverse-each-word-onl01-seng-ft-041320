66019621c7e377080a46fe9ffe9f5ec21c0d87d6
Juan Cobian <cobianjuan19@gmail.com>
2020-04-15 20:16:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,16 @@
 def reverse_each_word(string)
+<<<<<<< HEAD
   array = string.split(' ')
   empty_array = []
   array.collect do |word|
     empty_array << "#{word.reverse}"
   end
   empty_array.join(' ')
+=======
+  array = [].join(' ')
+  string.collect do |word|
+    array << "#{word.reverse}"
+  end
+  array
+>>>>>>> 5dafe2f0d19be33f059f614ba1c9b8fc01cd560d
 end
